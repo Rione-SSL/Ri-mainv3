bc4612ebd1c4de989f881bc9cfe451ddb3407682
mukuyo <adgjmptw47777@gmail.com>
2022-06-22 21:37:47 +0900

    :warning:straight kick error

--- src/modes/modeDrib.h
+++ src/modes/modeDrib.h
@@ -10,7 +10,7 @@ void before_drib() {
 
 void body_drib() {
     // モードのメインプログラムを書く関数.この関数がループで実行されます
-    float p = 1.0;
+    float p = 0.2;
     dribler.write(p);
     pc.printf("drib power:%f\r\n", p);
 }
