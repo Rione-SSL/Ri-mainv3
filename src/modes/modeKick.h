d4beb5beb0e8774c57811e0ae61ee024569e8b04
mukuyo <adgjmptw47777@gmail.com>
2022-08-31 19:07:46 +0900

    キッカーのテストプログラム書いた

--- src/modes/modeKick.h
+++ src/modes/modeKick.h
@@ -5,7 +5,7 @@
 
 void before_kick() { pc.printf("before Kicker Test\r\n"); }
 
-// KICKER_STRAIGHTでストレートキック
+// STRAIGHT_KICKERでストレートキック
 // CHIP_KICKERでチップキック
 void body_kick() {
     float p = 1.0;
@@ -15,8 +15,8 @@ void body_kick() {
         pc.printf(" KICK!!!");
         kicker[CHIP_KICKER].setPower(p); // power:0.0~1.0
         kicker[CHIP_KICKER].Kick();
-        // kicker[KICKER_STRAIGHT].setPower(p); // power:0.0~1.0
-        // kicker[KICKER_STRAIGHT].Kick();
+        // kicker[STRAIGHT_KICKER].setPower(p); // power:0.0~1.0
+        // kicker[STRAIGHT_KICKER].Kick();
     }
     pc.printf("\n");
 }
@@ -29,7 +29,7 @@ void after_kick() {
 
 const RIMode modeKick = {
     modeName : "mode_kick_test", //モードの名前.コンソールで出力したりLCDに出せます.
-    modeLetter : 'K', //モード実行のコマンド
+    modeLetter : 'K',            //モード実行のコマンド
     before : callback(before_kick),
     body : callback(body_kick),
     after : callback(after_kick)
--- src/modes/modes.h
+++ src/modes/modes.h
@@ -6,10 +6,10 @@
 #include "modeTestIMU.h"
 #include "modeDrib.h"
 #include "modeKick.h"
-
+#include "modeKick_force.h"
 // mode registration
-const int8_t modeIndex = 5;
+const int8_t modeIndex = 6;
 const RIMode modes[modeIndex] = {modeMain, modeTest, modeTestIMU, modeDrib,
-                                 modeKick};
+                                 modeKick, modeKickForce};
 
 #endif
